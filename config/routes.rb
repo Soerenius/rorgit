bfaae8a64eaf42e65e4b43eca0cc01d74dc79008
meiers <soeren.meier@htw-dresden.de>
2019-12-02 13:37:47 +0100

    KA

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :object_tables
   resources :root_tables
   ActiveAdmin.routes(self)
+  get "db" => "db#root_table"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
