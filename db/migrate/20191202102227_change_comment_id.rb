31f6be4ccb38d784e15b0112300cb57906571ce4
meiers <soeren.meier@htw-dresden.de>
2019-12-02 15:28:46 +0100

    push

--- db/migrate/20191202102227_change_comment_id.rb
+++ db/migrate/20191202102227_change_comment_id.rb
@@ -5,7 +5,7 @@ class ChangeCommentId < ActiveRecord::Migration[6.0]
     add_index :active_admin_comments, [:resource_type, :resource_id], name: 'index_active_admin_comments_on_resource_type_and_resource_id'
 
     remove_column :active_admin_comments, :author_id, type=:bigint
-    add_column :active_admin_comments, :author_id, :uuid
+    add_column :active_admin_comments, :author_id, :integer
     add_index :active_admin_comments, [:author_type, :author_id], name: 'index_active_admin_comments_on_author_type_and_author_id'
   end
 end
